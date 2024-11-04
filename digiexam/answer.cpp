2ccd7706dd962a5089076fc45ede35fdca5867b3
botdahl <botdahlstrom@gmail.com>
2024-11-04 13:35:50 +0100

    feat: question 6

--- digiexam/answer.cpp
+++ digiexam/answer.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+void f1();
+void f2();
+int f3();
+
 int main() {
   int *p;
 
@@ -9,5 +13,34 @@ int main() {
 
   std::cout << "Värdet på pekaren p: " << *p;
 
+
+  int *p2;
+  // p2 blir en dinglande pekare
+  p2 = f3();
+
   return 0;
+}
+
+// Question 6 - Minneshanteringsproblem
+// 1. Minnesläckor
+void f1() {
+  // den här frigörs aldrig
+  int *p = (int*) malloc(1);
+  return;
+}
+
+// 2. Dubbel Frigörning
+void f2() {
+  // den här frigörs två gånger
+  int *p = (int*) malloc(1);
+  free(p);
+  free(p);
+  return;
+}
+
+// 3. Dangling pointer
+int f3() {
+  // a finns inte i minne efter exec av funktionen
+  int a = 10;
+  return &a;
 }
